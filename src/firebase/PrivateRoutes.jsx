8af398e52a6ed40ccaccb79e7b61a59c76ebc170
Israt Jahan <isratbiswas28@gmail.com>
2025-09-22 10:46:24 +0600

    create private route

--- src/firebase/PrivateRoutes.jsx
+++ src/firebase/PrivateRoutes.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoutes = () => {
-  return <div></div>;
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return user ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
